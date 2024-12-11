64a08826bd60c5f236ef377786ef5d8e6ed916b0
Vinicius <shinokawaakutagawa@outlook.com>
2024-12-10 21:52:34 -0300

    Tirando funções desnecessarias

--- FluxEasy/Controllers/HomeController.cs
+++ FluxEasy/Controllers/HomeController.cs
@@ -6,13 +6,14 @@ namespace FluxEasy.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+            //_logger = logger;
+        //}
+        
         public IActionResult Index()
         {
             return View();
